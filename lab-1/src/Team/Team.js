177890387836e941b9739f4ac484bf7828c65f69
yehorbk <yehor.bk@gmail.com>
2022-10-08 21:43:36 +0300

    team section added

---
+++ lab-1/src/Team/Team.js
@@ -0,0 +1,31 @@
+import './Team.css';
+
+function Team() {
+  const team = [
+    {
+      name: 'Yehor Bublyk',
+      github: 'https://github.com/yehorbk',
+    },
+    {
+      name: 'Artem Bokii',
+      github: 'https://github.com/lightballer',
+    },
+    {
+      name: 'Dmytro Rekechynsky',
+      github: 'https://github.com/rocket111185',
+    },
+  ].map(member => {
+    const { github } = member;
+    member.username = github.substring(github.lastIndexOf('/') + 1);
+    return member;
+  });
+  return (
+    <div class="team-section">
+      <ul>
+        {team.map(({ name, github, username }) => <li key={name}>{name} | <a href={github}>{username}</a></li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default Team;
--- lab-1/src/index.js
+++ lab-1/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/App';
+import Team from './Team/Team';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Team />
   </React.StrictMode>
 );
