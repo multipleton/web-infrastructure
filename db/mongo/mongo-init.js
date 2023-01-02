9ae18aebd145826dd6499416e42c45f390c72149
yehorbk <yehor.bk@gmail.com>
2023-01-02 14:21:52 +0200

    databases added

---
+++ db/mongo/mongo-init.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+
+db.createCollection('contributions');
+
+db.contributions.insertMany([
+  {
+    shared_id: '053c7e56-306e-4ec8-a676-2a7be3d07928',
+    title: 'Wrote a book',
+    description: 'Really nice book, you should try it',
+  },
+  {
+    shared_id: '053c7e56-306e-4ec8-a676-2a7be3d07928',
+    title: 'Created a GitHub repository',
+    description: 'It\'s empty, but give him a chance',
+  },
+  {
+    shared_id: 'be851a64-e2ad-40db-8651-87786ededc86',
+    title: 'Woke up earlier than usual',
+    description: 'Yep, this is commendable',
+  },
+  {
+    shared_id: 'be851a64-e2ad-40db-8651-87786ededc86',
+    title: 'Took a photo of his cat',
+    description: 'What a cutie',
+  },
+]);
