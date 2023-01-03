30a09a4f3ef8ab39d2088c9198094f7510c10f0d
yehorbk <yehor.bk@gmail.com>
2023-01-03 15:00:31 +0200

    vagrant npm/node fixes added

--- provision.sh
+++ provision.sh
@@ -1,16 +1,37 @@
 #!/bin/sh
 
-# update and install packages
-apt -y update
-apt -y install nodejs nginx
+# Update and install packages
+apt-get -y update || exit 1
+apt-get -y install nginx || exit 1
 
-# install npm dependencies and build the project
-npm install
-npm run build
+# Install NVM (Node.js version manager)
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash
 
-# move nginx config and build
-cp ./nginx/default.conf ./etc/nginx/conf.d/default.conf
-cp ./lab-1/build ./usr/share/nginx/lab-1
+# Export NVM namespace
+export NVM_DIR="$HOME/.nvm"
 
-# start nginx service
-service nginx start 
+# This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Switch Node.js version
+nvm install v16
+nvm use v16
+
+# Move to /vagrant
+cd /vagrant
+
+# Install npm dependencies
+npm install || exit 1
+
+# Build the project
+npm run build || exit 1
+
+# Move nginx config and build
+cp ./nginx/default.conf /etc/nginx/conf.d/default.conf
+cp -r ./build /usr/share/nginx/lab-1
+
+# Start nginx service
+service nginx start
